e335596baa166a97a5353ef451e267b8d0f4c3a7
mxrodriguez04 <103071498+mxrodriguez04@users.noreply.github.com>
2022-04-05 22:58:42 -0700

    Initial Commit

---
+++ Github Test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Github Test
+//
+//  Created by Manuel Rodriguez on 4/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
